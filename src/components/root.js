cae2496e35264a20cac5083fa9eec59bb0c9377e
Igor Vieira <igor.p.r.vieira@gmail.com>
2018-03-30 09:53:38 -0300

    import firebase structure pass store on provider and sync history with react-router-redux using syncHistoryWithStore

--- src/components/root.js
+++ src/components/root.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react'
-import {browserHistory} from 'react-router'
+import { browserHistory } from 'react-router'
 import Routes from './routes'
-class Root extends Component {
+import { init as firebaseInit } from 'javascript/firebase'
+import { Provider } from 'react-redux'
+import configureStore from './configureStore'
+import { syncHistoryWithStore } from 'react-router-redux'
+
+export default class Root extends Component {
   constructor(props) {
     super(props)
+    firebaseInit()
+    this.store = configureStore(browserHistory)
   }
-render() {
+  render() {
     return (
-      <Routes history={browserHistory}/>
+      <Provider store={this.store}>
+        <Routes history={syncHistoryWithStore(browserHistory, this.store)}/>
+      </Provider>
     )
   }
 }
-export default Root
