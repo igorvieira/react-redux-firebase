742878fcc7b9fde2cc68aaed742b06c1fd7e27e5
Igor Vieira <igor.p.r.vieira@gmail.com>
2018-03-30 09:55:02 -0300

    config router and pass components for any Route our use for pass a component and a path

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,11 +1,33 @@
-import {Router, Route} from 'react-router'
+import {Router, Route, IndexRoute, Redirect} from 'react-router'
 import App from './App'
-import About from './App/about'
-import React from 'react'
+import React, {Component} from 'react'
+import ShowTodo from './App/show-todo'
+import {connect} from 'react-redux'
+import {loadSections} from '../actions/todo'
+import './App/assets/normalize.min.css'
+import './App/assets/style.css'
 
-export default (props) => (
-  <Router {...props}>
-    <Route path='/' component={App} />
-    <Route path='/about' component={About} />
-  </Router>
-)
+class Routes extends Component {
+  constructor(props) {
+    super(props)
+    this.props.loadSections()
+  }
+  render() {
+    return (
+      <div className="todo">
+        <a href="/">
+          <h1 className="todo__title">
+            Hey Human =]
+          </h1>
+        </a>
+        <Router {...this.props}>
+          <Route path='/'>
+            <IndexRoute component={App} />
+            <Route path=':id' component={ShowTodo}/>
+          </Route>
+        </Router>
+      </div>
+    )
+  }
+}
+export default connect(null, {loadSections})(Routes)
