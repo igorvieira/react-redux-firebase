b44f5bb8f72c46db2a63725d526eefdc4850a2b4
Igor Vieira <igor.p.r.vieira@gmail.com>
2018-03-30 08:05:44 -0300

    initial commit && CRA

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from 'components/root'
+import './index.css';
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+);
